c729c503a6308670ddd946ca4cdd71888deb5b20
Nicholas Villarreal <nvillarreal@protonmail.com>
2025-06-02 21:31:31 -0600

    blur input after submitting

--- src/assets/js/youtube-search.js
+++ src/assets/js/youtube-search.js
@@ -20,6 +20,7 @@ export class YouTubeSearch extends LitElement {
                     composed: true,
                 }),
             );
+            e.target.elements.search.blur();
         }
     }
 
